7abc8502ac1a007c48afd7f8d0f081e9a6795f46
michele <michifilip2006@gmail.com>
2025-05-04 10:20:31 +0200

    Update dependencies and remove Flowbite integration from the project

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,11 +1,3 @@
-<script>
-  import { Button } from 'flowbite-svelte';
-</script>
-  
-<h1 class="text-3xl font-bold text-blue-600">Hello SvelteKit + Tailwind + Flowbite!</h1>
-  
-<Button color="primary">Flowbite Button</Button>
-  
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<h1 class="underline">ciao</h1>
+<div class="bg-red-400"><h1>xiaoooo</h1></div>
